608237dd0880a472638fb9ad8996b6ba864928d9
np-suneel <np.suneel@outlook.com>
2020-11-02 03:23:54 +0530

    Initial commit

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.srchLbl = new System.Windows.Forms.Label();
+            this.srchBx = new System.Windows.Forms.TextBox();
+            this.GoBtn = new System.Windows.Forms.Button();
+            this.srcCodeBx = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.DownBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // srchLbl
+            // 
+            this.srchLbl.AutoSize = true;
+            this.srchLbl.Location = new System.Drawing.Point(108, 44);
+            this.srchLbl.Name = "srchLbl";
+            this.srchLbl.Size = new System.Drawing.Size(44, 13);
+            this.srchLbl.TabIndex = 0;
+            this.srchLbl.Text = "Search:";
+            // 
+            // srchBx
+            // 
+            this.srchBx.Location = new System.Drawing.Point(171, 40);
+            this.srchBx.Name = "srchBx";
+            this.srchBx.Size = new System.Drawing.Size(68, 20);
+            this.srchBx.TabIndex = 1;
+            // 
+            // GoBtn
+            // 
+            this.GoBtn.Location = new System.Drawing.Point(268, 38);
+            this.GoBtn.Name = "GoBtn";
+            this.GoBtn.Size = new System.Drawing.Size(30, 24);
+            this.GoBtn.TabIndex = 2;
+            this.GoBtn.Text = "Go";
+            this.GoBtn.UseVisualStyleBackColor = true;
+            this.GoBtn.Click += new System.EventHandler(this.GoBtn_Click);
+            // 
+            // srcCodeBx
+            // 
+            this.srcCodeBx.Location = new System.Drawing.Point(427, 100);
+            this.srcCodeBx.Multiline = true;
+            this.srcCodeBx.Name = "srcCodeBx";
+            this.srcCodeBx.ReadOnly = true;
+            this.srcCodeBx.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.srcCodeBx.Size = new System.Drawing.Size(368, 339);
+            this.srcCodeBx.TabIndex = 3;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 100);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(387, 339);
+            this.webBrowser.TabIndex = 4;
+            // 
+            // DownBtn
+            // 
+            this.DownBtn.Location = new System.Drawing.Point(543, 36);
+            this.DownBtn.Name = "DownBtn";
+            this.DownBtn.Size = new System.Drawing.Size(133, 29);
+            this.DownBtn.TabIndex = 5;
+            this.DownBtn.Text = "Download emotes";
+            this.DownBtn.UseVisualStyleBackColor = true;
+            this.DownBtn.Click += new System.EventHandler(this.DownBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 455);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 14);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "©AnonDevs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 478);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DownBtn);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.srcCodeBx);
+            this.Controls.Add(this.GoBtn);
+            this.Controls.Add(this.srchBx);
+            this.Controls.Add(this.srchLbl);
+            this.Name = "Form1";
+            this.Text = "Emote Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label srchLbl;
+        private System.Windows.Forms.TextBox srchBx;
+        private System.Windows.Forms.Button GoBtn;
+        private System.Windows.Forms.TextBox srcCodeBx;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button DownBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        string srcCode;
+        string currUrl;
+        string folname;
+        string fullFileName;
+        public Form1()
+        {
+            InitializeComponent();
+            this.AcceptButton = GoBtn;
+            this.srcCodeBx.AutoSize = true;
+            this.srcCodeBx.ReadOnly = true;
+            this.srcCodeBx.Multiline = true;
+            webBrowser.ScriptErrorsSuppressed = true;
+            DownBtn.Hide();
+        }
+
+
+        private void GoBtn_Click(object sender, EventArgs e)
+        {
+
+            string query = srchBx.Text;
+            if (string.IsNullOrWhiteSpace(srchBx.Text))
+            {
+                MessageBox.Show("Enter a search term.");
+                srchBx.Focus();
+            }
+            else
+            {
+                string webs = "https://twitchemotes.com/search?query=" + query;
+                webBrowser.Navigate(webs);
+                webBrowser.DocumentCompleted += WebBrowser_DocumentCompleted;
+
+            }
+        }
+
+        private void WebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.AbsolutePath != (sender as WebBrowser).Url.AbsolutePath)
+                return;
+            
+            CurrUrlFunc();
+        }
+
+        private static string RetrieveData(string url)
+        {
+
+            // used to build entire input
+            var sb = new StringBuilder();
+
+            // used on each read operation
+            var buf = new byte[8192];
+            try
+            {
+                // prepare the web page we will be asking for
+                var request = (HttpWebRequest)
+                                         WebRequest.Create(url);
+
+                /* Using the proxy class to access the site
+                 * Uri proxyURI = new Uri("http://proxy.com:80");
+                 request.Proxy = new WebProxy(proxyURI);
+                 request.Proxy.Credentials = new NetworkCredential("proxyuser", "proxypassword");*/
+
+                // execute the request
+                var response = (HttpWebResponse)
+                                           request.GetResponse();
+
+                // we will read data via the response stream
+                Stream resStream = response.GetResponseStream();
+
+                string tempString = null;
+                int count = 0;
+
+                do
+                {
+                    // fill the buffer with data
+                    count = resStream.Read(buf, 0, buf.Length);
+
+                    // make sure we read some data
+                    if (count != 0)
+                    {
+                        // translate from bytes to ASCII text
+                        tempString = Encoding.ASCII.GetString(buf, 0, count);
+
+                        // continue building the string
+                        sb.Append(tempString);
+                    }
+                } while (count > 0); // any more data to read?
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(@"Failed to retrieve data from the network. Please check you internet connection: " +
+                                exception);
+            }
+            return sb.ToString();
+        }
+        
+        
+
+        private void DownBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+
+                srcCodeBx.Text = "Getting image URLs...";
+
+                DownImgFunc();
+                    srcCodeBx.Text += Environment.NewLine + "Done. Check your Desktop for respective folder.";
+                
+               
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Unkown error occured. Plese reopen the program.");
+            }
+
+        }
+
+
+        void CurrUrlFunc()
+        {
+
+            currUrl = webBrowser.Url.ToString();
+            if (Regex.Match(currUrl, ".*/search|(.*/)$").Success)
+                MessageBox.Show("Make sure you click on the streamer name or an emote to download");
+            else
+            {
+                srcCode = RetrieveData(currUrl);
+                DownBtn.Show();
+            }
+        }
+
+        
+
+        void DownImgFunc()
+        {
+
+            srcCodeBx.Text += Environment.NewLine + Environment.NewLine+ "Downloading images...";
+            string namepattern = "<h3><a href=\".*\".*>(.*)</a></h3>";
+            string imgpattern = "<img\\s.*?src=(?:'|\")([^'\">]+)(?:'|(2.0)\") .* data-regex=\"(.*)\"";
+
+            Match namatch = Regex.Match(srcCode, namepattern);
+
+            if (namatch.Success)
+            {
+                folname = namatch.Groups[1].Value.ToString();
+            }
+            else
+                srcCodeBx.Text += Environment.NewLine + Environment.NewLine + "Name not found";
+
+
+
+            string desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            System.IO.Directory.CreateDirectory(desktopFolder + "\\" + folname);
+
+
+            MatchCollection matches = Regex.Matches(srcCode, imgpattern);
+
+            foreach (Match match in matches)
+            {
+
+                fullFileName = Path.Combine(desktopFolder + "\\" + folname, match.Groups[3].Value + ".png");
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(match.Groups[1].Value.ToString() + "3.0", fullFileName);
+                }
+
+            }
+        }
+    }
+}
